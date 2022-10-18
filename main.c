2f638de63468d750369a2a89a78b04f86c3d948c
Jovem <zeluiz.gamer@hotmail.com>
2022-10-18 19:41:53 -0300

    adicionando a função soma (x,y)

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-
-
 void somatoria(int x, int y){
    int i;
    long int soma = 0;
@@ -10,16 +8,12 @@ void somatoria(int x, int y){
    }
    printf("\nA somatória é %ld\n",soma);
 }
-
 int main(void) {
-  
    printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
    printf("\n\nDigite o número x: ");
    scanf("%d",&x);
    printf("\n\nDigite o número y: ");
    scanf("%d",&y);
-
    soma(x,y);
-
  return 0;
 }
